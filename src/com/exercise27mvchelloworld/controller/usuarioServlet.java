41b8af6b4c0c251e7b2f0f8e4cdf7284f659b4f1
carlos-java-182 <carlos.villasenor@outlook.com>
2019-11-01 15:40:57 -0600

    Actual project

--- src/com/exercise27mvchelloworld/controller/usuarioServlet.java
+++ src/com/exercise27mvchelloworld/controller/usuarioServlet.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.exercise27mvchelloworld.modelo.UsuarioModelo;
+
 
 @WebServlet("/usuarioServlet")
 public class usuarioServlet extends HttpServlet{
@@ -15,18 +17,40 @@ public class usuarioServlet extends HttpServlet{
    
    	protected void doPost(HttpServletRequest request, HttpServletResponse response)
    			throws ServletException, IOException {
+   	
+   		
+   		
 	
 		String sid="";
+		int iid=0;
 		String sname="";
 		String sage="";
+		short shage=0;
 		
 		sid = request.getParameter("txtId");
 		sname = request.getParameter("txtName");
 		sage = request.getParameter("txtAge");
 		
+		UsuarioModelo myUser = new UsuarioModelo (sid,sname,sage);
+		
 		response.getWriter().append("The id input is:"+sid);
+		iid=Integer.parseInt(sid);
 		response.getWriter().append("The name input is:"+sname);
 		response.getWriter().append("The age input is:"+sage);
+		shage = Short.parseShort(sage);
+		
+		UsuarioModelo myUser1 = new UsuarioModelo(iid, sname, shage);
+		UsuarioModelo myUser2 = new UsuarioModelo();
+		myUser2.setId(iid);
+		myUser2.setName(sname);
+		myUser2.setAge(shage);
+		
+		response.getWriter().append("The id input is:"+sid);
+		response.getWriter().append("The name imput is:"+sname);
+		response.getWriter().append("The age imput is:"+shage);
+		
+		
+	
 	}
 
 }
--- src/com/exercise27mvchelloworld/modelo/UsuarioModelo.java
+++ src/com/exercise27mvchelloworld/modelo/UsuarioModelo.java
@@ -2,4 +2,39 @@ package com.exercise27mvchelloworld.modelo;
 
 public class UsuarioModelo {
 
+	public int edad = 0;
+	public String nombre = "";
+	public int id = 0;
+	
+	
+	public int getEdad(){
+		return edad;
+	}
+	
+	public void setEdad(int edad) {
+		this.edad= edad;
+	}
+	
+	
+	public String getNombre(){
+		return  nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre= nombre;
+	}
+	
+	public int getId(){
+		return  id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	
+
+
+	
+	
 }
